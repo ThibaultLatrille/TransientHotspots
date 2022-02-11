40b5e02031b7cb6b07336946b4543322d376a063
Thibault Latrille <thibault.latrille@ens-lyon.org>
2022-02-11 09:56:12 +0100

    Added presentation part1-3 and merged pdf.

--- docs/merge_pdf.sh
+++ docs/merge_pdf.sh
@@ -1,1 +1,1 @@
-pdftk A=Reu_equipe_11.02.2022.pdf B=BPGE-11-02-2022.pdf cat A1-19 B A20-31 output combined.pdf
+pdftk A=BPGE-11-02-2022-part1-3.pdf B=BPGE-11-02-2022-part2.pdf cat A1-19 B A20-31 output combined.pdf
